603a80239f3731b6de145ec4d98782d3bee9d480
Peter Jones <pjones@devalot.com>
2019-10-29 15:36:15 -0700

    Update dependency: zxcvbn-hs

--- crypto/default.nix
+++ crypto/default.nix
@@ -4,23 +4,21 @@
 let
   nix-hs-src = fetchGit {
     url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
+    rev = "a2b666faf8cb3c6f769655dfb36f4695f78bc3c3";
   };
 
   nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-  zxcvbn-hs-src = {
-    url = "https://code.devalot.com/sthenauth/zxcvbn-hs.git";
-    rev = "07a224794f0299e4c066056841485e2d86d8b075";
-  };
-
-  zxcvbn-hs = import "${fetchGit zxcvbn-hs-src}/default.nix" { inherit pkgs; };
 
 in nix-hs {
   cabal = ./iolaus-crypto.cabal;
 
   overrides = lib: self: super: with lib; {
-    inherit zxcvbn-hs;
     iolaus-opaleye = import ../opaleye { inherit pkgs; };
+
+    zxcvbn-hs = lib.fetchGit {
+      url = "https://code.devalot.com/sthenauth/zxcvbn-hs.git";
+      rev = "7c05b0c91b4b7f98777cf83ba5b24cdc1d62bfcd";
+      ref = "next";
+    };
   };
 }
